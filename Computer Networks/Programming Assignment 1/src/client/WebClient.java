f7eb751a67d0339d3a2fb61aaeac5696b8394f19
Brittany Blassingill <brittany.blassingill@gmail.com>
2018-02-16 22:16:50 -0600

    Updated client code to communicate via Socket

--- Computer Networks/Programming Assignment 1/src/client/WebClient.java
+++ Computer Networks/Programming Assignment 1/src/client/WebClient.java
@@ -3,19 +3,60 @@ package client;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.HttpURLConnection;
+import java.net.Socket;
 import java.net.URL;
+import java.net.UnknownHostException;
 
 public class WebClient {
     private final String USER_AGENT = "Mozilla/5.0";
     public static void main(String[] args) throws Exception {
-        WebClient test = new WebClient();
-        System.out.println("Testing GET request");
-        test.sendGet();
+//        WebClient test = new WebClient();
+//        System.out.println("Testing GET request");
+//        test.sendGet();
+        String hostName = "localhost";
+        int portNumber = 8443;
+
+        try {
+            Socket clientSocket = new Socket(hostName, portNumber);
+            PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+            BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+
+            out.println("GET /TestPDFDocument.pdf HTTP/1.1");
+            //out.println("Host: localhost:8443");
+            out.println("Connection: Close");
+            out.println();
+
+
+            String response;
+            while ((response = in.readLine()) != null) {
+                System.out.println(response);
+            }
+
+            in.close();
+            out.close();
+
+//            String userInput;
+//            while ((userInput = stdIn.readLine()) != null) {
+//                out.println(userInput);
+//                System.out.println("echo: " + in.readLine());
+//            }
+        }
+
+        catch (UnknownHostException e) {
+            System.err.println("Don't know about host " + hostName);
+            System.exit(1);
+        } catch (IOException e) {
+            System.err.println("Couldn't get I/O for the connection to " +
+                    hostName);
+            System.exit(1);
+        }
     }
 
+
     private void sendGet() throws Exception {
-        String url = "http://localhost:8443/TestTextDocument.txt";
+        String url = "http://localhost:8443/TestWordDocument.txt";
         URL obj = new URL(url);
         HttpURLConnection con = (HttpURLConnection) obj.openConnection();
         con.setRequestProperty("User-Agent", USER_AGENT);
