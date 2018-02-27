db39c372e72f72e33e365f934549ca2ef9f27649
Brittany Blassingill <brittany.blassingill@gmail.com>
2018-02-26 22:29:50 -0600

    finished letter 2 digit subroutine

--- Data Mining/Project1/dataHandler.py
+++ Data Mining/Project1/dataHandler.py
@@ -1,6 +1,8 @@
 from sklearn.model_selection import train_test_split
 import csv
 import numpy as np
+import scipy.io as io
+import string
 
 def pickDataClass(filename, class_ids) :
 	#class_ids: contains the classes to be picked. ex: (3, 5, 8, 9)
@@ -41,14 +43,81 @@ def splitData2TestTrain(filename, number_per_class, test_instances) :
 
 	return X_train, y_train, X_test, y_test
 
-# def	storeTrainXTrainY :
+def	storeTrainXTrainY(X_train, y_train, X_test, y_test, filetype) :
+	# file = open("test.txt", "w")
+	# file.writelines(str(X_train.toList()))
+	# file.close
 
+	# test = np.concatenate((y_train[:, np.newaxis], X_train), axis=1)
+	train_data = np.column_stack((X_train, y_train)).transpose()
+	train_data = np.flip(train_data, 0)
+	
+	test_data = np.column_stack((X_test, y_test)).transpose()
+	test_data = np.flip(test_data, 0)
 
-# def letter_2_digit_convert(charArray) :
+	# arr = np.arange(9)
+	# arr = arr.reshape((3, 3))
+	# print(arr)
+	# print()
+	# print()
 
+	# print(train_data)
 
+	if(filetype == "matlab"):
+		# file = open("trainData.txt", "w")
+		# file2 = open("testData.txt", "w")
+
+		mat = np.asmatrix(train_data)
+		# print(mat.tolist())
+
+		io.savemat("trainData.mat", mdict={"trainData": mat.tolist()})
+		io.savemat("testData.mat", mdict={"testData": test_data.tolist()})
+
+		# print(train_data.type())
+		mat = io.loadmat("trainData.mat")
+		print(mat.items())
+		# for test in mat.values():
+		# 	print(test)
+		# print(mat.values())
+		# print(mat.type())
+		# for row in mat:
+		# 	print(row)
+		# 	for col in mat[row]:
+		# 		print(mat[row][col])
+		# print(str(mat.tolist()))
+	if(filetype == "python"):
+		file = open("trainData.py", "w")
+		file2 = open("testData.py", "w")
+		file.writelines(str(train_data.tolist()))
+		file.close
+		file2.writelines(str(test_data.tolist()))
+		file2.close 
+	if(filetype == "text"):
+		file = open("trainData.txt", "w")
+		file2 = open("testData.txt", "w")
+		file.writelines(str(train_data.tolist()))
+		file.close
+		file2.writelines(str(test_data.tolist()))
+		file2.close
+
+	# file.writelines(str(train_data.tolist()))
+	# file.close
+	# file2.writelines(str(test_data.tolist()))
+	# file2.close
+
+def letter_2_digit_convert(charString) :
+	resultArray = []
+
+	for char in charString:
+		char = char.lower()
+		resultArray.append(string.ascii_lowercase.index(char) + 1)
+
+	return resultArray
+		
 def main() :
 	# subsetData = pickDataClass('ATNT50/trainDataXY.txt', [1, 3])
 	X_train, y_train, X_test, y_test = splitData2TestTrain('ATNT50/trainDataXY.txt', 9, 5)
+	# storeTrainXTrainY(X_train, y_train, X_test, y_test, "matlab")
+	letter_2_digit_convert("ACFG")
 
 main()	
